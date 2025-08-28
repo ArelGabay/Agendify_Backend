b003d9d6126dab9e320bb0bc3d2c712c2ba33cd8
ArelGabay <arelgabay1@gmail.com>
2025-08-28 17:38:29 +0300

    Update CORS configuration to allow multiple origins

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(express.urlencoded({ extended: true }));
 // CORS Configuration
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173/", "https://agendifyx.up.railway.app/"],
     methods: "GET, POST, PUT, DELETE, OPTIONS",
     allowedHeaders: "Content-Type, Authorization",
     credentials: true,
