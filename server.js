22463664b19fd682883fb6ac725a09403674a2b6
ArelGabay <arelgabay1@gmail.com>
2025-08-28 18:18:44 +0300

    Fix CORS origin URL by removing trailing slash

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(express.urlencoded({ extended: true }));
 // CORS Configuration
 app.use(
   cors({
-    origin: ["http://localhost:5173/", "https://agendifyx.up.railway.app/", "frontend.railway.internal"],
+    origin: ["http://localhost:5173/", "https://agendifyx.up.railway.app", "frontend.railway.internal"],
     methods: "GET, POST, PUT, DELETE, OPTIONS",
     allowedHeaders: "Content-Type, Authorization",
     credentials: true,
